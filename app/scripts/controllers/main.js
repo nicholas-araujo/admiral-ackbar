9f238154f5751892aa92f5ccd369dc45aaf4dd3d
m4rd <nicholas-araujo@users.noreply.github.com>
2016-03-01 11:29:09 +0000

    project admiral ackbar first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+/**
+ * @ngdoc function
+ * @name admiralAckbarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the admiralAckbarApp
+ */
+
+/*jslint white: true */
+/*global angular */
+angular.module('admiralAckbarApp')
+  .controller('MainCtrl', function ($scope) {
+    'use strict';
+
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3'
+    ];
+  });
