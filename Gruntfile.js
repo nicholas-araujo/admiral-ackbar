af8f35c2d066cd236efb6005e261a8ba197996bc
m4rd <nicholas-araujo@users.noreply.github.com>
2016-03-07 00:51:36 +0000

    setup up tests with karma and jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function (grunt) {
 
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin',
-    ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
+    sprite: 'grunt-spritesmith',
+    foo: '@abc/grunt-foo',        // for private modules.
+    bar: 'custom/bar.js'          // for custom tasks.
   });
 
   // Configurable paths for the application
@@ -220,7 +220,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,12 +12,7 @@ angular.module('admiralAckbarApp')
   .controller('MainCtrl', function ($scope) {
     'use strict';
 
-    $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
-    ];
+    $scope.todos = [];
 
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
